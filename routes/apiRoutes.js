72c4f0c457dfa8dadabf09b743946b3f26fa4480
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-30 00:50:32 -0600

    delete route not working,  css not working properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,18 +5,18 @@ const fs = require('fs');
 var db = require('../db/db.json');
 
 
-        //GET ROUTE TO READ THE db.json FILE AND RETURN ALL SAVED NOTES 
-        //GET route = server to client
+//GET ROUTE TO READ THE db.json FILE AND RETURN ALL SAVED NOTES 
+//GET route = server to client
 app.get('/notes', (req, res) => {
   db = JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8'))
   res.json(db);
 });
 
 
-    //POST ROUTE TO SAVE NEW NOTE TO THE BODY, ADD TO db.json AND RETURN A NEW NOTE TO THE CLIENT
-    //POST route = client to server
-    //This allows users to populate the server with data by sending data from the client side of the application to the server
-    //Use Insomnia Core to test routes
+//POST ROUTE TO SAVE NEW NOTE TO THE BODY, ADD TO db.json AND RETURN A NEW NOTE TO THE CLIENT
+//POST route = client to server
+//This allows users to populate the server with data by sending data from the client side of the application to the server
+//Use Insomnia Core to test routes
 app.post('/notes', (req, res) => {
   var newNote = req.body;
   db.push(newNote);
@@ -25,7 +25,7 @@ app.post('/notes', (req, res) => {
     res.status(400).json({ errors: errors });
     return;
   }
-})
+});
 
 
 
@@ -43,14 +43,25 @@ app.get('/notes/:id', (req, res) => {
 
 
 //DELETE ROUTE
-app.delete
+app.delete('/notes/:id', (req, res) => {
+  const params = [req.params.id];
+
+  db.query(params, (err, result) => {
+    if(err){
+      res.statusMessage(400).json({ error: res.message });
+      res.json({
+        message: 'Note not found'
+      });
+    } else {
+      res.json ({
+        message: 'deleted',
+        id: req.params.id
+      });
+    }
+  });
+});
 
   
               
-
-
-
-
-
-
+//EXPORT THE APP
 module.exports = app;
