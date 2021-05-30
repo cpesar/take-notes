4f23ebc513a60ff072f2f7f14f3d0405169b8ed4
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-29 21:23:12 -0600

    adding routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 var db = require('../db/db.json');
 
 
-
+        //GET ROUTE TO READ THE db.json FILE AND RETURN ALL SAVED NOTES 
 app.get('/notes', (req, res) => {
   db = JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8'))
   res.json(db);
+});
+
+
+    //POST ROUTE TO SAVE NEW NOTE TO THE BODY, ADD TO db.json AND RETURN A NEW NOTE TO THE CLIENT
+app.post('/notes', (req, res) => {
+
 })
 
 module.exports = app;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ var path = require("path");
 var express = require("express");
 var app = express();
 
-                  //GET ROUTE FOR notes.html
+            //GET ROUTE TO RETURN notes.html FILE
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-                  //GET ROUTE FOR index.html
+            //GET ROUTE TO RETURN index.html FILE
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
